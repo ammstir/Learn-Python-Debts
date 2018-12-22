5ec00ef3ab9de39a39acf59530e3a9f81b0911e4
Anna <ammstir@gmail.com>
2018-12-22 13:01:03 +0300

    Update settings.py

--- debts/settings.py
+++ debts/settings.py
@@ -15,10 +15,10 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
+SECRET_KEY = ''
 # SECURITY WARNING: keep the secret key used in production secret!
 # in local_settings
 
@@ -123,3 +123,4 @@ try:
     from .local_settings import *
 except:
     pass
+
