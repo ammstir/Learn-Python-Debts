6a77f47caec0bedec34b72ef69a0d30a977b461a
Anna <ammstir@gmail.com>
2019-01-31 18:08:05 +0300

    temp commit

--- debt_history/views.py
+++ debt_history/views.py
@@ -4,6 +4,7 @@ from .forms import LoginForm, AddBill
 
 def make_bill(request):
     form = AddBill(request.POST)
+    new_bill = form.save()
     return render(request, 'debt_history/make_debts.html', {'form': form})
 
 
@@ -11,5 +12,20 @@ def make_login(request):
     login = LoginForm(request.POST)
     return render(request, 'debt_history/make_login.html', {'form': login})
 
+# Create your views here.
+# def make_debt(request):
+#     if request.method == 'POST':
+#         form = LoginForm(request.POST)
+#         if not form.is_valid:
+#             return render(request, 'debt_history/make_debt.html', {'form': form})
+#
+#         # debt = Debts(
+#         #
+#         #  )
+#         # bills = Bill
+#         # return
+#
+#      form = LoginForm()
+#     return render(request, 'debt_history/make_debt.html', {'form': form})
 
 
