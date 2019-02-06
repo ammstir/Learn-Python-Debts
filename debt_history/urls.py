5276c18288b5d4faf2b3dfce32df38519cb06385
Anna <ammstir@gmail.com>
2019-02-06 17:02:18 +0300

    urls update

--- debt_history/urls.py
+++ debt_history/urls.py
@@ -4,6 +4,6 @@ from .views import make_bill, make_login, debt_list, bill_list
 urlpatterns = [
     path('bills/new/', make_bill, name='make_bill'),
     path('login/', make_login, name='make_login'),
-    path('debtlist/', debt_list, name='debt_list'),
-    path('billslist/', bill_list, name='bill_list'),
+    path('debt/list/', debt_list, name='debt_list'),
+    path('bills/list/', bill_list, name='bill_list'),
 ]
