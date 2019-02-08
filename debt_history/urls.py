889400387a07c25e4195fa2e49952cf1a73afc04
Olya Golf <olya.golf@gmail.com>
2019-02-08 17:41:09 +0300

    Добавила дашбоард, сделала наследование, вывела всё что можно и удалила ненужный код

--- debt_history/urls.py
+++ debt_history/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import registration, logout_view, make_bill, debt_list, bill_list, add_group
+from .views import registration, logout_view, make_bill, debt_list, bill_list, add_group, group_list
 
 
 urlpatterns = [
@@ -9,6 +9,7 @@ urlpatterns = [
     path('bills/new/', make_bill, name='make_bill'),
     path('debt/list/', debt_list, name='debt_list'),
     path('bills/list/', bill_list, name='bill_list'),
+    path('group/list/', group_list, name='group_list'),
     #path('', main )
 
 ]
